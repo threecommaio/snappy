57ebff10c2bc0b9fefe6056f4a54dfcfb4c7063b
Chris Goffinet <chris@threecomma.io>
2018-07-19 18:32:47 -0400

    Add support for progress bar output instead of per file output

--- pkg/snappy/snappy.go
+++ pkg/snappy/snappy.go
@@ -4,11 +4,16 @@ import (
 	"log"
 	"os"
 
+	"github.com/cheggaaa/pb"
 	humanize "github.com/dustin/go-humanize"
 )
 
 // Backup a nodes snapshot to S3
 func Backup(config *AWSConfig, snapshotID string) {
+	var (
+		totalSize int64
+	)
+
 	s3, err := NewS3(config)
 	if err != nil {
 		log.Fatal(err)
@@ -20,20 +25,30 @@ func Backup(config *AWSConfig, snapshotID string) {
 		log.Println("snapshot already exists, going to continue upload anyway")
 	}
 
-	var totalSize int64
-
 	files := cassandra.GetSnapshotFiles(snapshotID)
-	for path, key := range files {
-		log.Println("Uploading file:", key)
-		if err := s3.UploadFile(path, key); err != nil {
-			log.Fatal(err)
-		}
 
+	for path := range files {
 		fi, e := os.Stat(path)
 		if e != nil {
 			log.Fatal(e)
 		}
 		totalSize += fi.Size()
 	}
+
+	bar := pb.New64(totalSize).Start()
+	bar.SetUnits(pb.U_BYTES)
+	bar.ShowSpeed = true
+
+	for path, key := range files {
+		if err := s3.UploadFile(path, key); err != nil {
+			log.Fatal(err)
+		}
+		fi, e := os.Stat(path)
+		if e != nil {
+			log.Fatal(e)
+		}
+		bar.Add64(fi.Size())
+	}
+	bar.Finish()
 	log.Println("Uploaded a total size of:", humanize.Bytes(uint64(totalSize)))
 }
