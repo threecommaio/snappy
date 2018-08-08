5d000435599d1330d3f6ca8c3c2ba6a0c790022b
Chris Goffinet <chris@threecomma.io>
2018-08-08 19:25:55 -0400

    Use strings.TrimPrefix instead and filepath.Join instead of custom Sprintf

--- pkg/snappy/cassandra.go
+++ pkg/snappy/cassandra.go
@@ -154,8 +154,8 @@ func (c *Cassandra) GetSnapshotFiles(id string) map[string]string {
 						log.Fatal(err)
 					}
 					if !info.IsDir() {
-						remotePath := strings.TrimLeft(path, tableDir)
-						s3Files[path] = fmt.Sprintf("backups/%s/%s/%s/%s/%s", id, node, keyspace, table, remotePath)
+						remotePath := strings.TrimPrefix(path, tableDir)
+						s3Files[path] = filepath.Join("backups", id, node, keyspace, table, remotePath)
 					}
 					return nil
 				})
